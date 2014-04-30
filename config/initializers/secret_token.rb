2f8971a0dc20afb5f710fb9bb871b02d45fffdda
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2014-04-30 15:07:31 +0000

    Initial commit of a new generaed rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GovspeakPreview::Application.config.secret_token = '2c74f59fa5764c27cd88a582e01a6ce0ac0d21cf36eb144a15de83c245f11b79bfc19b1680f2a28bd8e43ef4ea0a84c4b99b72041cd0d519a82acb9a81ba2aa7'
