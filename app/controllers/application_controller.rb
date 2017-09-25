f4876ef370bd7c9521f5fc095a58af2097122585
Tijmen Brommet <tijmen@gmail.com>
2017-09-25 17:02:48 +0100

    Upgrade styles
    - Use components
    - Remove H1's because they won't be rendered by govspeak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include Slimmer::Headers
-
-  # before_filter :set_slimmer_template
-
-  # def set_slimmer_template
-  # 	# set_slimmer_headers(template: "header_footer_only")
-  # end
-
+  include Slimmer::GovukComponents
 end
