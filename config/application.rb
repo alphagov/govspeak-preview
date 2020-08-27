bf2825da1dca64935e29b7b2cee336241dc059eb
Kevin Dew <kevindew@me.com>
2020-08-27 12:41:14 +0100

    Modernise rubocop and fix issues

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'sprockets/railtie'
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- spec/services/google_docs_to_govspeak_spec.rb
+++ spec/services/google_docs_to_govspeak_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe GoogleDocsToGovspeak do
     it "converts to govspeak" do
       report = File.open("spec/support/fixtures/Sample Assessment Report.zip")
 
-      govspeak = GoogleDocsToGovspeak.new(report).to_govspeak
+      govspeak = described_class.new(report).to_govspeak
 
       expect(govspeak).to eql(File.read("spec/services/expected-markdown-sample-assessment.md"))
     end
