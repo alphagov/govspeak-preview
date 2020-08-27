e389e318a1c1643ffa8f5485451bec6b9f83c808
Kevin Dew <kevindew@me.com>
2020-08-27 11:59:58 +0100

    Clean up gems
    This removes gems that aren't used and removes versioning pins of all but
    Rails (as per GOV.UK convention, see [1]).
    
    As part of this the asset compression approach has been updated to
    support libsass.
    
    [1]: https://github.com/alphagov/government-frontend/commit/e716934edc88b0064b89189db15b409887114549#diff-8b7db4d5cc4b8f6dc8feb7030baa2478

--- Gemfile
+++ Gemfile
@@ -2,27 +2,23 @@ source "https://rubygems.org"
 
 ruby File.read(".ruby-version").chomp
 
-gem "gds-api-adapters"
+gem "rails", "5.2.3"
+
 gem "govspeak"
 gem "govuk_publishing_components"
 gem "puma"
-gem "rails", "5.2.3"
-gem "reverse_markdown", "~> 1.0.5"
-gem "rubyzip", "~> 1.3.0"
-
-group :assets do
-  gem "coffee-rails"
-  gem "sass-rails", "~> 5.1"
-  gem "uglifier"
-end
+gem "reverse_markdown"
+gem "rubyzip"
+gem "sassc-rails"
+gem "uglifier"
 
 group :development, :test do
   gem "capybara"
   gem "listen"
-  gem "rspec-rails", "~> 3.7"
+  gem "rspec-rails"
   gem "rubocop-govuk"
 end
 
 group :test do
-  gem "webmock", "~> 3.3.0"
+  gem "webmock"
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,12 @@ module GovspeakPreview
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Using a sass css compressor causes a scss file to be processed twice
+    # (once to build, once to compress) which breaks the usage of "unquote"
+    # to use CSS that has same function names as SCSS such as max.
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
