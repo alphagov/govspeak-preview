f343d0272a88889dac49a91e51560bf4f27a9547
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-04-28 10:56:02 +0100

    Run rails app:update

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,8 +2,4 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-
-# Once this has app has been deployed a while with hybrid we can switch it to
-# :json - I set this is in August 2020, so if it is at at least a few weeks
-# after that this can be changed to :json
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :json
