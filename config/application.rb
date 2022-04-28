d22a1ad67e30f82a9090670dea4f8af9dc44c8e7
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-04-28 11:48:40 +0100

    Change SCSS compression approach
    This is a known problem, as seen in other GOV.UK applications:
    - https://github.com/alphagov/govuk-frontend/issues/1350
    - https://github.com/alphagov/finder-frontend/issues/1124
    - https://github.com/alphagov/whitehall/commit/1ae8699230617783533979d77b9d71a54198b752
    - https://github.com/alphagov/govuk-account-manager-prototype/commit/86b4a8e76fef6cac41800f5e1f86041b39728084

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module GovspeakPreview
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Using a sass css compressor causes a scss file to be processed twice (once
+    # to build, once to compress) which breaks the usage of "unquote" to use
+    # CSS that has same function names as SCSS such as max
+    config.assets.css_compressor = nil
   end
 end
