fe130a90c90f0cddb857995e11012cd258125d55
Mike Patrick <mike.patrick1@digital.cabinet-office.gov.uk>
2024-07-11 09:42:02 +0100

    Move components that use ES6 to a separate file
    This change is part of our preparation for upgrading to a version of the
    govuk_publishing_components gem that includes the new GOV.UK Frontend V5.1
    
    It's one of the steps in the Upgrading to GOV.UK Frontend v5
    document[^1] and refers to the "Stop Internet Explorer 11 and other
    older browsers running unsupported JavaScript" section in GOV.UK
    Frontend v5 release notes[^2].
    
    Some of our publishing components are progressively enhanced using
    JavaScript and a subset of those enhancements use ES6 features. We want
    to prevent browsers that don't support ES6 from having to load and
    attempting to use our components' ES6 JavaScript.
    
    We call browsers that don't support ES6 Grade X[^3] and those browsers
    also don't support the type=”module” attribute in script tags. So, we
    use type=”module” to achieve preventing the ES6 JavaScript being used by
    Grade X browsers.
    
    [^1]: https://docs.google.com/document/d/1uwip7pzQwM7t5ghn9_8KrsWXLePSRUltFPZ5fYw6Ab8/edit?pli=1
    [^2]: https://github.com/alphagov/govuk-frontend/releases/tag/v5.0.0
    [^3]: https://frontend.design-system.service.gov.uk/browser-support/#grade-x

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/govspeak
 
 //= require paste-html-to-markdown
