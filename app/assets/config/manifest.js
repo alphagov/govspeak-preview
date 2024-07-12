ef6c866a37a9f26eb8f31c9ba5d9e5a32600d37f
Mike Patrick <mike.patrick1@digital.cabinet-office.gov.uk>
2024-07-12 12:31:59 +0100

    Revert "Move components that use ES6 to a separate file"
    Apparently I was too keen and shouldn't have merged this before the new
    (GOV.UK Frontend V5.1-bundling) version of the
    govuk_publishing_components gem was available.
    
    This reverts commit 3390209c838b1df451059b736ce9e98f22f4f924.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_tree ../images
 //= link application.js
-//= link es6-components.js
 //= link application.css
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/govspeak
 
 //= require paste-html-to-markdown
