badfa46e25c790146ad347292eefe9e9a6ea7c55
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-10-17 15:15:48 +0100

    Add healthcheck endpoint
    This is required by EKS before a pod is used.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
+
   constraints(host: /govspeak-preview\.herokuapp\.com/) do
     match "/(*path)" => redirect { |params, _req| "https://govspeak-preview.publishing.service.gov.uk/#{params[:path]}" }, via: %i[get post]
   end
