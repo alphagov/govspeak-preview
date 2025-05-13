036420842c89342671aa02c07d1e34b7274b9ea1
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2025-05-13 11:32:53 +0100

    Add rescue to stop CSRF errors being logged to sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActionController::InvalidAuthenticityToken do
+    render_error(
+      403,
+      body: "Invalid authenticity token",
+    )
+  end
 end
