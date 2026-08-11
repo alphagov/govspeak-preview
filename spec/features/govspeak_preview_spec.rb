e3fc93cf4431ab783a63da220ed53a38950658ea
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2026-08-11 14:59:12 +0100

    Remove references to `govuk-govspeak` class
    Unrelated change that was also packaged into the above upgrade
    
    https://github.com/alphagov/govuk_publishing_components/pull/5629

--- spec/features/govspeak_preview_spec.rb
+++ spec/features/govspeak_preview_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Govspeak preview", type: :feature do
     fill_in "govspeak_input", with: "## Ut viverra ante nunc, quis efficitur leo volutpat et"
     click_on "Preview"
 
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak h2", text: "Ut viverra ante nunc, quis efficitur leo volutpat et")
+    expect(page).to have_css(".gem-c-govspeak h2", text: "Ut viverra ante nunc, quis efficitur leo volutpat et")
   end
 
   scenario "Add Govspeak for one title and one paragraph" do
@@ -17,8 +17,8 @@ RSpec.feature "Govspeak preview", type: :feature do
       \n\nUt mattis augue sed nisl tincidunt, a laoreet lectus consequat. Mauris lacinia venenatis urna eu rhoncus."
     click_on "Preview"
 
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak h2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak p", text: "Ut mattis augue sed nisl tincidunt, a laoreet lectus consequat. Mauris lacinia venenatis urna eu rhoncus.")
+    expect(page).to have_css(".gem-c-govspeak h2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+    expect(page).to have_css(".gem-c-govspeak p", text: "Ut mattis augue sed nisl tincidunt, a laoreet lectus consequat. Mauris lacinia venenatis urna eu rhoncus.")
   end
 
   scenario "Add Govspeak for one paragraph and three list items" do
@@ -30,11 +30,11 @@ RSpec.feature "Govspeak preview", type: :feature do
       \n- Mauris egestas, turpis a lobortis fringilla, orci neque pellentesque tellus, a rutrum sem lorem ac nibh."
     click_on "Preview"
 
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak p", text: "Pellentesque ut facilisis eros, eget viverra nulla.")
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak li", count: 3)
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak li:nth-child(1)", text: "Sed porta et nisi et tempus")
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak li:nth-child(2)", text: "Nullam nec quam fringilla, imperdiet sem nec, feugiat urna. In hac habitasse platea dictumst")
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak li:nth-child(3)", text: "Mauris egestas, turpis a lobortis fringilla, orci neque pellentesque tellus, a rutrum sem lorem ac nibh.")
+    expect(page).to have_css(".gem-c-govspeak p", text: "Pellentesque ut facilisis eros, eget viverra nulla.")
+    expect(page).to have_css(".gem-c-govspeak li", count: 3)
+    expect(page).to have_css(".gem-c-govspeak li:nth-child(1)", text: "Sed porta et nisi et tempus")
+    expect(page).to have_css(".gem-c-govspeak li:nth-child(2)", text: "Nullam nec quam fringilla, imperdiet sem nec, feugiat urna. In hac habitasse platea dictumst")
+    expect(page).to have_css(".gem-c-govspeak li:nth-child(3)", text: "Mauris egestas, turpis a lobortis fringilla, orci neque pellentesque tellus, a rutrum sem lorem ac nibh.")
   end
 
   scenario "Add Govspeak for one paragraph and one link" do
@@ -43,7 +43,7 @@ RSpec.feature "Govspeak preview", type: :feature do
     fill_in "govspeak_input", with: "[Nulla finibus finibus hendrerit](https://www.lipsum.com/feed/html). In hac habitasse platea dictumst. Nam volutpat quam sed tellus ullamcorper ultrices. Maecenas hendrerit dolor lacus, sed commodo lectus sodales sed."
     click_on "Preview"
 
-    expect(page).to have_css(".gem-c-govspeak.govuk-govspeak p", text: "Nulla finibus finibus hendrerit. In hac habitasse platea dictumst. Nam volutpat quam sed tellus ullamcorper ultrices. Maecenas hendrerit dolor lacus, sed commodo lectus sodales sed.")
+    expect(page).to have_css(".gem-c-govspeak p", text: "Nulla finibus finibus hendrerit. In hac habitasse platea dictumst. Nam volutpat quam sed tellus ullamcorper ultrices. Maecenas hendrerit dolor lacus, sed commodo lectus sodales sed.")
     expect(page).to have_link("Nulla finibus finibus hendrerit", href: "https://www.lipsum.com/feed/html")
   end
 
@@ -59,7 +59,7 @@ RSpec.feature "Govspeak preview", type: :feature do
     click_on "Preview"
 
     expect(page).to have_css(
-      ".gem-c-govspeak.govuk-govspeak a.footnote",
+      ".gem-c-govspeak a.footnote",
       text: "[troednodyn 1]",
     )
   end
