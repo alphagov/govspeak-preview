3db45a9d400509961f2231aed3747b01b95de3d5
Lucas Mbiwe <lucas.mbiwe@dsit.gov.uk>
2026-08-21 16:58:17 +0100

    Bump govuk_publishing_components to v68.3.0
    The Navigation team asked for this as part of their work to update the
    govuk_publishing_components gem to v6 of the design system and to ensure
    all of our software is secure and up to date in terms of features.
    
    - Update govuk_publishing_components to v68.3.0, because it contains
      govuk-frontend v6 (latest major version of the design system)
    - Follow import and require advice from /component-guide
    - Update calls to govuk-colour() to get rid of deprecation warnings
    - Add .govuk-frontend-supported to the body element to prevent a
      warning [1] from showing up in the console
    - Update yarn.lock
    
    [1] https://github.com/alphagov/govuk-frontend/blob/26679248834fd3cae37dfeb494e6f0c765fcc86e/packages/govuk-frontend/src/govuk/errors/index.jsdom.test.mjs#L42-L46
    Closes [PP-7946](https://gov-uk.atlassian.net/browse/PP-7946)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,10 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/file-upload
 //= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/service-navigation
+//= require govuk_publishing_components/components/skip-link
 
 //= require paste-html-to-markdown
 //= require html-govspeak
